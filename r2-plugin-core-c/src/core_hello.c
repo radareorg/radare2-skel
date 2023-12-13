d6f706265ba2c47b8edc85ce47eaec6d58d7a46f
pancake <pancake@nopcode.org>
2023-12-13 16:28:02 +0100

    Add core plugin example

---
+++ r2-plugin-core-c/src/core_hello.c
@@ -0,0 +1,34 @@
+
+/* radare - Copyright 2023 - yourname */
+
+#define R_LOG_ORIGIN "core.hello"
+
+#include <r_core.h>
+
+static int r_cmd_r2ai_client(void *user, const char *input) {
+	RCore *core = (RCore *) user;
+	if (r_str_startswith (input, "hello")) {
+		r_cons_printf ("world\n");
+		return true;
+	}
+	return false;
+}
+
+// PLUGIN Definition Info
+RCorePlugin r_core_plugin_hello = {
+	.meta = {
+		.name = "core-hello",
+		.desc = "hello world from an r2core plugin",
+		.author = "pancake",
+		.license = "MIT",
+	},
+	.call = r_cmd_r2ai_client,
+};
+
+#ifndef R2_PLUGIN_INCORE
+R_API RLibStruct radare_plugin = {
+	.type = R_LIB_TYPE_CORE,
+	.data = &r_core_plugin_hello,
+	.version = R2_VERSION
+};
+#endif
