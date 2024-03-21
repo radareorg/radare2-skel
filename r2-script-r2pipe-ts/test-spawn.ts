5bb023edafb6401bfaa2dcb152ffa048b76690a5
pancake <pancake@nopcode.org>
2024-03-21 23:14:15 +0100

    Update package.json file

--- r2-script-r2pipe-ts/test-spawn.ts
+++ r2-script-r2pipe-ts/test-spawn.ts
@@ -4,7 +4,7 @@ async function main() {
   console.log("Hello R2Pipe for TypeScript");
   // const r2 = await r2pipe.open("http://127.0.0.1:9090");
   const r2 = await r2pipe.open("/bin/ls"); // create new r2 instance
-  const r2 = await r2pipe.open(); // from r2
+//  const r2 = await r2pipe.open(); // from r2
   const res = await r2.cmd("?E Hello TypeScript");
   console.log(res);
   await r2.quit();
