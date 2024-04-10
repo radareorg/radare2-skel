d7ce8aefd39a5d9834782920d8e1d2fcc49fbcd4
pancake <pancake@nopcode.org>
2024-04-10 09:41:30 +0200

    Initial import of an r2pipe-swift example

---
+++ r2-script-r2pipe-swift/Package.swift
@@ -0,0 +1,22 @@
+// swift-tools-version: 5.10
+import PackageDescription
+
+let pacakge = Package(
+        name: "r2pipe-example",
+        products: [
+             .executable(name: "r2pipeExample", targets: ["r2pipeExample"]),
+        ],
+        dependencies: [
+                .package(
+                    url: "https://github.com/radareorg/radare2-r2pipe", revision: "1.0.4"
+                ),
+        ],
+        targets: [
+            .executableTarget(
+                name: "r2pipeExample",
+                dependencies: [
+                    .product(name: "r2pipe", package: "radare2-r2pipe")
+                ]
+            )
+        ]
+)
---
+++ r2-script-r2pipe-swift/Sources/main.swift
@@ -0,0 +1,13 @@
+import Foundation
+
+/*
+// is 'r2pipe.swiftmodule' needed to import r2pipe?
+$ swiftc -emit-module -module-name r2pipe Sources/r2pipe/r2pipe*
+
+import r2pipe
+if let r2p = r2pipe.R2Pipe("#!ccall") {
+    print("Testint ccall")
+}
+*/
+
+print("Hello Swift")
