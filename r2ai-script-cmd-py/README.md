69faea0ca4daad7a7c40b6088204c3fd4a3a008b
pancake <pancake@nopcode.org>
2024-04-20 04:13:31 +0200

    Initial import of the r2ai plugin

---
+++ r2ai-script-cmd-py/README.md
@@ -0,0 +1,5 @@
+# r2ai plugin
+
+r2ai can be extended in python.
+
+This directory contains a program that interacts with r2 and r2ai.
---
+++ r2ai-script-cmd-py/plugin.py
@@ -0,0 +1,13 @@
+import r2ai
+
+print("Hello World from r2ai")
+try:
+  res = r2_cmd("?e hello world from r2").strip()
+  print(f"RES({res})")
+  res = runline(None, "-m")
+  print(f"RES({res})")
+  res = runline2(None, "-m")
+  print(f"RES({res})")
+  ¢ print(f"RES({ais})")
+except:
+  traceback.print_exc()
